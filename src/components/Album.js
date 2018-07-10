f2c80d8f394f0bea7649d32682667276e26ce297
Evan Medeiros <kumbionics@gmail.com>
2018-07-10 19:08:06 -0400

    "update album page"

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import albumData from './../data/albums';
 
 
- class Album extends Component {
+class Album extends Component {
   constructor(props) {
     super(props);
 
     const album = albumData.find( album => {
-      return album.slug === this.props.match.params.slug
-    });
+       return album.slug === this.props.match.params.slug
+     });
 
     this.state = {
       album: album
@@ -19,12 +19,13 @@ import albumData from './../data/albums';
     return(
       <section className="album">
         <section id="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+        <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
           <div className="album-details">
-          <h1 id="album-title">{this.state.album.title}</h1>
+           <h1 id="album-title">{this.state.album.title}</h1>
              <h2 className="artist">{this.state.album.artist}</h2>
              <div id="release-info">{this.state.album.year} {this.state.album.label}</div>
-          </div>
+
+            </div>
         </section>
         <table id="song-list">
           <colgroup>
@@ -32,8 +33,7 @@ import albumData from './../data/albums';
             <col id="song-title-column" />
             <col id="song-duration-column" />
           </colgroup>
-          <tbody>
-          </tbody>
+          <tbody>{this.state.album.songs}</tbody>
         </table>
       </section>
     );
--- src/components/Library.js
+++ src/components/Library.js
@@ -6,13 +6,15 @@ import albumData from './../data/albums';
 class Library extends Component {
     constructor(props) {
         super(props);
-        this.state = {albums: albumData};
+        this.state = {
+          albums: albumData
+        };
     }
     render() {
         return(
             <section className="library">
                 {
-                  this.state.albums.map( (album,index) => 
+                  this.state.albums.map( (album,index) =>
                   <Link to={`/album/${album.slug}`} key={index}>
                         <img src={album.albumCover} alt={album.title}/>
                         <div>{album.title}</div>
