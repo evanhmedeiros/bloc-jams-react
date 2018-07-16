1fb0614b8c1668b73227757005b1f8bd7c9c925d
Evan Medeiros <kumbionics@gmail.com>
2018-07-16 00:04:22 -0400

    Checkpoint 5 components: library: refactor

--- src/components/Library.js
+++ src/components/Library.js
@@ -3,26 +3,29 @@ import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
 
 
+
 class Library extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-          albums: albumData
+             albums: albumData 
         };
     }
+    
     render() {
         return(
             <section className="library">
-                {
-                  this.state.albums.map( (album,index) =>
-                  <Link to={`/album/${album.slug}`} key={index}>
+            {
+                this.state.albums.map( (album, index) => 
+                    <Link to={'/album/${album.slug}'} key={index}>
                         <img src={album.albumCover} alt={album.title}/>
                         <div>{album.title}</div>
                         <div>{album.artist}</div>
                         <div>{album.songs.length} songs</div>
                     </Link>
-                  )
-                }
+                )
+            }
             </section>
         );
     }
